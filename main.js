c61f975e42465ebe7b21f2543ee077ef6148d58a
juanpedropuig <67132645+juanpedropuig@users.noreply.github.com>
2020-11-12 00:48:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,125 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+
+
+// Add your functions below:
+
+// When truthy, array contains digits of a valid credit cars number.
+const validateCred = arr => {
+  // Drops the last number of the array.
+  const lastNumber = arr.pop();
+  const reversedArr = [];
+  
+  // Reverse array and multiply by 2 every other number.
+  for (let i=arr.length-1;i>=0;i--) {
+    if (i % 2 === 0) {
+      let doubledValue = arr[i] * 2;
+      if (doubledValue > 9) {
+        doubledValue -= 9;
+        reversedArr.push(doubledValue);
+      } else {
+        reversedArr.push(doubledValue);
+      }
+    } else {
+      reversedArr.push(arr[i]);
+    }
+  }
+  // Summed all numbers together.
+  const reducedArr = reversedArr.reduce((acc, currVal) => acc + currVal) + lastNumber;
+  if (reducedArr % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+// Checks through nested arrays and returns invalid numbers.
+const findInvalidCards = nestedArr => {
+  const invalidNested = [];
+  // Pushes to the invalidNested the arrays that are falsy.
+  nestedArr.forEach(arr => {
+    if (!validateCred(arr)) {
+      invalidNested.push(arr);
+    }
+  });
+
+  return invalidNested;
+};
+
+
+
+// Identifies the credit card companies that issued these faulty numbers.
+const idInvalidCardCompanies = invalidNestedArr => {
+  const invalidCompanies = [];
+
+  // Checks the first number of the arrays to identify the company.
+  invalidNestedArr.forEach(arr => {
+    switch (arr[0]) {
+      case 3:
+        // Checks if the company was already added to the array so they won't duplicate.
+        const amex = invalidCompanies.indexOf('Amex');
+        if (amex === -1) {
+          invalidCompanies.push('Amex');
+        }
+        break;
+      case 4:
+        const visa = invalidCompanies.indexOf('Visa');
+        if (visa === -1) {
+          invalidCompanies.push('Visa');
+        }
+        break;
+      case 5:
+        const mastercard = invalidCompanies.indexOf('Mastercard');
+        if (mastercard === -1) {
+          invalidCompanies.push('Mastercard');
+        }
+        break;
+      case 6:
+        const discover = invalidCompanies.indexOf('Discover');
+        if (discover === -1) {
+          invalidCompanies.push('Discover');
+        }
+        break;
+      default:
+        console.log('Company not found.');
+    }
+  });
+
+  return invalidCompanies;
+};
+
+
+
+
+
+
+const invalidArr = findInvalidCards(batch);
+console.log(invalidArr);
+const compan = idInvalidCardCompanies(invalidArr);
+console.log(compan);
+
+
+
+
